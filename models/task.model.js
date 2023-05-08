45f4b29c353bd76327266ac658f93e11371393af
Jordi Galobart Salvat <jgalobart@gmail.com>
2023-05-08 19:14:39 +0200

    Create Task Model

--- models/task.model.js
+++ models/task.model.js
@@ -6,7 +6,21 @@ const Schema = mongoose.Schema;
 // Schema defines the STRUCTURE of documents in the collection
 // this is the BLUEPRINT for all instances
 const taskSchema = new Schema({
-  
-});
+    task: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    dueDate: Date,
+    status: {
+        type: String,
+        required: true,
+        default: "PENDING",
+        enum: ["COMPLETED", "IN PROGRESS", "PENDING", "POSTPONED", "DELETED"]
+        },
+    deletedAt: Date
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Task", taskSchema);
